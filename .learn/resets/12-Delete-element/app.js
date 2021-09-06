9ea6f32db35613ef4303245ed92e4cea6c3fd6e8
silvinaas <silvinaas4@gmail.com>
2021-09-06 23:11:00 +0000

    avance

--- .learn/resets/12-Delete-element/app.js
+++ .learn/resets/12-Delete-element/app.js
@@ -2,17 +2,7 @@ var people = ['juan','ana','michelle','daniella','stefany','lucy','barak'];
 
 function deletePerson(personName)
 {
-let people2 = [];
 	//your code here
-	for (let i = 0; i<people.length; i++)
-	{
-		if (people[i] == personName){
-		
-			people.splice(personName, 1);
-			
-		}
-	}
-	return people;
 }
 
 console.log(deletePerson('daniella'));
--- exercises/14-Divide-and-Conquer/app.js
+++ exercises/14-Divide-and-Conquer/app.js
@@ -1,6 +1,19 @@
 let list_of_numbers = [4, 80, 85, 59, 37,25, 5, 64, 66,	81,20, 64, 41, 22, 76,76, 55, 96, 2, 68];
 
 // your code here
+function mergeTwoList(arreglo){
+    let listadenum = [];
+    let odd = [];
+    let even = [];
+    for (var item in list_of_numbers){
+        if (item%2==0){
+            even.push(item);
+        }
+        else{
+            odd.push(item);
+        }
+        }
+    }
 
 
 console.log(mergeTwoList(list_of_numbers))
