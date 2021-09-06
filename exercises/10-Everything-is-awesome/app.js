fad2226a15cd145001b64d187129a630d20edf7b
silvinaas <silvinaas4@gmail.com>
2021-09-06 01:42:38 +0000

    avance ejercicios

--- exercises/10-Everything-is-awesome/app.js
+++ exercises/10-Everything-is-awesome/app.js
@@ -3,6 +3,12 @@ let myArray = [ 1, 0, 0, 0, 1, 0, 0, 0, 1, 1 ];
 const ZerosToYahoos = (arr) => {
     let return_array = [];
     arr.forEach((item,index) => {
+        if (item == 0){
+            return_array.push("Yahoo")
+        }
+        else{
+            return_array.push(1);
+        }
             // magic goes inside these brackets
     });
     return return_array;
